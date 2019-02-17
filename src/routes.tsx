57a280067a35a3b9d1ab205b47d9a7b380f343b5
Lauren <laurayco@outlook.com>
2019-02-17 00:11:32 -0600

    added board component to routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import { Route } from 'react-router-dom';
 import Home from "./components/home";
+import BoardComponent from "./components/board";
 
 export default <React.Fragment>
     <Route exact path="/" component={Home}/>
+    <Route path="/board/:slug" component={BoardComponent} />
 </React.Fragment>;
